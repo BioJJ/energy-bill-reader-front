c62207454f00639a3e79075b6514e3f6a7763719
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-07-31 19:15:57 -0400

    feat: ✨ calling components on pages

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Container } from './style'
+import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput'
 
 const List: React.FC = () => {
-	return <h1> List</h1>
+	const options = [{ value: 'jefferson', label: 'jefferson' }]
+
+	const handleMonthSelected = (month: string) => {
+		console.log(month)
+	}
+	return (
+		<Container>
+			<ContentHeader title={'historico'} lineColor={'#4E41F0'}>
+				<SelectInput
+					options={options}
+					onChange={(e) => handleMonthSelected(e.target.value)}
+					defaultValue={1}
+				/>
+				{/* <SelectInput
+					options={years}
+					onChange={(e) => handleYearSelected(e.target.value)}
+					defaultValue={yearSelected}
+				/> */}
+			</ContentHeader>
+		</Container>
+	)
 }
 
 export default List
