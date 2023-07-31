542ad6b8622bcf827453b6f5b724c7cbd1ec520f
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-07-31 19:12:37 -0400

    feat: ✨ implementing aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react'
-import { Container } from './style'
+import logoImg from '../../assets/logo.svg'
+
+import {
+	MdDashboard,
+	MdArrowDownward,
+	MdArrowUpward,
+	MdExitToApp,
+	MdClose,
+	MdMenu
+} from 'react-icons/md'
+
+import {
+	Container,
+	Header,
+	LogImg,
+	Title,
+	MenuContainer,
+	MenuItemLink,
+	MenuItemButton,
+	ToggleMenu,
+	ThemeToggleFooter
+} from './style'
 
 const Aside: React.FC = () => {
 	return (
-		<Container>
-			<h1> Aside</h1>
+		<Container menuIsOpen={false}>
+			<Header>
+				{/* <ToggleMenu onClick={handleToggleMenu}>
+					{toggleMenuIsOpened ? <MdClose /> : <MdMenu />}
+				</ToggleMenu> */}
+
+				<LogImg src={logoImg} alt="Logo Energy Bill" />
+				<Title>Energy Bill</Title>
+			</Header>
+
+			<MenuContainer>
+				<MenuItemLink href="/">
+					<MdDashboard />
+					Dashboard
+				</MenuItemLink>
+
+				<MenuItemLink href="/list/entry-balance">
+					<MdArrowUpward />
+					Historico
+				</MenuItemLink>
+
+				{/* <MenuItemLink href="/list/exit-balance">
+					<MdArrowDownward />
+					Saídas
+				</MenuItemLink> */}
+
+				<MenuItemButton>
+					<MdExitToApp />
+					Sair
+				</MenuItemButton>
+			</MenuContainer>
 		</Container>
 	)
 }
